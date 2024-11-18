0160bb04af1bd23b84d6bb876d3ea7286ae30d4e
OrryFrasetyo <orrytio12345@gmail.com>
2024-11-18 22:45:43 +0700

    setup tabel produk

--- database/migrations/2024_11_16_154902_create_produks_table.php
+++ database/migrations/2024_11_16_154902_create_produks_table.php
@@ -14,17 +14,27 @@ return new class extends Migration
         Schema::create('produks', function (Blueprint $table) {
             $table->id();
             $table->string('nama_produk');
+            //tambahkan list kategori
             $table->unsignedBigInteger('category_id');
             $table->decimal('harga', 15, 2);
             $table->decimal('fee_penjualan', 15, 2)->nullable(); // 21% dari harga
             $table->decimal('dana_diterima', 15, 2)->nullable(); // harga - fee_penjualan
             $table->decimal('berat', 8, 2);
             $table->integer('stok');
-            $table->json('dimensi_barang'); // Panjang, lebar, tinggi dalam format JSON
+            $table->json('dimensi_barang');
+            $table->boolean('packing_kayu');
+            $table->boolean('asuransi');
+            // Panjang, lebar, tinggi dalam format JSON
             $table->json('gambar'); // Array JSON untuk 4 gambar
             $table->string('kondisi_barang');
-            $table->integer('garansi')->default(0);
-            $table->text('deskripsi_produk');
+            $table->string('garansi')->default(false);
+            $table->string('lama_pemakaian');
+            $table->integer('tangan_ke');
+            $table->string('waktu_pembelian');
+            $table->string('minus');
+            $table->string('kelengkapan');
+            $table->string('wireless');
+            $table->string('suara_aman');
             $table->timestamps();
 
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
