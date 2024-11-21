75b538f07ff6259a2753db1aceb2ae15bcf3843c
Riansyah Divano <riansyahdivano29@gmail.com>
2024-11-22 00:41:16 +0700

    viewlistproduk

---
+++ resources/views/listproduk.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-900">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <!-- Ganti skrip Font Awesome dengan CDN alternatif -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" rel="stylesheet" />
+    <title>SigmaGem Consign</title>
+</head>
+
+<body class="h-full">
+
+    <!-- Navbar -->
+    <x-navbar class="x-navbar"></x-navbar>
+
+</body>
+
+</html>
