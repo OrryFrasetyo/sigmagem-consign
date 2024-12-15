f5b05ba09d43a0bdcd18df5e23428655093357c4
OrryFrasetyo <orrytio12345@gmail.com>
2024-12-16 02:00:42 +0700

    80% table transaction admin

---
+++ app/Enums/ProductPayment.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Enums;
+
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+
+enum ProductPayment: string implements HasLabel, HasColor, HasIcon
+{
+    case TERTUNDA = 'tertunda';
+    case MENUNGGU_KONFIRMASI = 'menunggu_konfirmasi';
+    case SUKSES = 'sukses';
+    case GAGAL = 'gagal';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::TERTUNDA => 'Tertunda',
+            self::MENUNGGU_KONFIRMASI => 'Menunggu Konfirmasi',
+            self::SUKSES => 'Sukses',
+            self::GAGAL => 'Gagal',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::TERTUNDA => 'gray',
+            self::MENUNGGU_KONFIRMASI => 'warning',
+            self::SUKSES => 'success',
+            self::GAGAL => 'danger',
+        };
+    }
+
+    public function getIcon(): ?string
+    {
+        return match ($this) {
+            self::TERTUNDA => 'heroicon-o-stop',
+            self::MENUNGGU_KONFIRMASI => 'heroicon-m-arrow-path-rounded-square',
+            self::SUKSES => 'heroicon-c-check',
+            self::GAGAL => 'heroicon-o-exclamation-triangle',
+        };
+    }
+}
